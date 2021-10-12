39fca6d430c68cf8bbb1065894425290536731e6
Khachik Shahzadyan <qwerity@gmail.com>
2021-10-13 01:15:13 +0400

    beincoin/bitcoin send transaction

--- test/bic/test_beincoind_client.js
+++ test/bic/test_beincoind_client.js
@@ -2,10 +2,20 @@ const {beincoind} = require("../../common/utils");
 
 (async() => {
   try {
-    const balance = await beincoind.getBalance()
-    console.log(balance)
-    const address = await beincoind.getNewAddress()
-    console.log(address)
+    console.log("balance: ", await beincoind.getBalance())
+    console.log("unconfirmedbalance: ", await beincoind.getUnconfirmedBalance())
+
+    const address = await beincoind.getNewAddress('ksh')
+    console.log("new address:", address)
+
+    const txid = await beincoind.sendToAddress(address, 0.0001)
+    // with txid we can check the transaction status by confirmations number
+    const tx = await beincoind.getTransaction(txid)
+    console.log("txid:", txid)
+    console.log("\tfee:", tx.fee)
+    console.log("\tblockhash:", tx.blockhash)
+    console.log("\tconfirmed: ", tx.confirmations > 21, ", confirmations: ", tx.confirmations)
+
   } catch(e) {
     console.log(e)
   }
--- test/btc/test_bitcoind_client.js
+++ test/btc/test_bitcoind_client.js
@@ -1,12 +1,15 @@
-const {bitcoind} = require("../../common/utils");
+const {bitcoind, beincoind} = require("../../common/utils");
 
 (async() => {
   try {
-    const balance = await bitcoind.getBalance()
-    console.log("balance: ", balance)
+    console.log("balance: ", await beincoind.getBalance())
+    console.log("unconfirmedbalance: ", await beincoind.getUnconfirmedBalance())
+
     const address = await bitcoind.getNewAddress({label: 'ksh'})
     console.log("new address:", address)
+
     const txid = await bitcoind.sendToAddress(address, 0.0001)
+    // with txid we can check the transaction status by confirmations number
     const tx = await bitcoind.getTransaction(txid)
     console.log("txid:", txid)
     console.log("\tfee:", tx.fee)
